a035cdad35c8ac280408dc95ceeee4f36fef6c80
vishvAsaH <vishvas.vasuki@gmail.com>
2017-05-29 09:45:07 -0700

    Finalize key, avoid dumping excessively long keys - can cause trouble with URLs in browsers.

--- src/main/scala/sanskrit_coders/db/couchbaseLite/CouchbaseLiteDb.scala
+++ src/main/scala/sanskrit_coders/db/couchbaseLite/CouchbaseLiteDb.scala
@@ -66,7 +66,7 @@ class CouchbaseLiteDb(val d: Database) {
       if (key.length <= 750) {
         return key
       } else {
-        return key.substring(0, 749) + key.substring(750).hashCode().toString
+        return key.substring(0, 749) + "__rest_hashed__" + key.substring(750).hashCode().toString
       }
     }
     val finalKey = finalizeKey(key)
